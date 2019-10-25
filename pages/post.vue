cc3a72b42793725b3c464e12172433c258b39839
SuzukiDaishi <zukky.rikugame@gmail.com>
2019-10-25 11:00:08 +0900

    投稿できたらHomeへ

--- pages/post.vue
+++ pages/post.vue
@@ -90,6 +90,7 @@ export default {
                 this.textData = ''
                 this.placeData = ''
                 this.nameData = ''
+                this.$router.push('/')
             })
 
         }
